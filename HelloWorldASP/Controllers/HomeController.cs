9809eb7916969dcbe0c3588a4adc9f8cb95a6318
Anh-Toine <Nick1234567890a@gmail.com>
2021-08-27 08:36:07 -0400

    First commit

---
+++ HelloWorldASP/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FutureValue.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWorldASP.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // ViewBag.Name = "Marry";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+}
